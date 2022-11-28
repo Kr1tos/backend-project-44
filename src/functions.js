2e7d9d802b8b6baa66bdf4df1604d07368e392cc
Kr1tos <kritos.biz@gmail.com>
2022-11-28 20:23:32 +0300

    Prime

--- src/functions.js
+++ src/functions.js
@@ -5,7 +5,7 @@ class func {
     getRandomNumber (min, max){
         let rand = min - 0.5 + Math.random() * (max - min + 1);
         return Math.round(rand);
-    }
+    };
 
     calculate(operands, operator){
         switch (operator) {
@@ -20,11 +20,41 @@ class func {
 
     isEven = (number) => number % 2 == 0;
 
-    gcd = (a, b) => {
+    gcd(a, b) {
         if (b === 0) {
             return a
-            }
-            return new func().gcd(b, a % b)
         }
+        return new func().gcd(b, a % b)
+    };
+
+    gemerateProgression(first, step, length){
+        const nums= [];
+        let count = 0;
+        while(count != length) {
+            const number = first + step * count;
+            nums.push(number);
+            count +=1;
+        }
+        return nums
+    };
+    putMissingValue = (missingValue, progression) => {
+        const newProgression = [];
+      
+        for (let index = 0; index < progression.length; index += 1) {
+            if (index === missingValue) {
+                newProgression.push('..');
+            } else {
+                newProgression.push(progression[index]);
+            }
+        }
+        return newProgression.join(' ');
+
+    };
+    isPrime(num) {
+        for(var i = 2; i < num; i++)
+            if(num % i === 0) return false;
+        return true;
+    };
 }
+
 export default new func();
---
+++ src/games/brainPrime.js
@@ -0,0 +1,12 @@
+import start from '../index.js';
+import func from '../functions.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const game = () => {
+    const quest = func.getRandomNumber(1,50);
+    const correctAnsw = func.isPrime(quest) ? 'yes' : 'no';
+
+    return [quest, correctAnsw];
+}
+
+export default () => start(description, game);
