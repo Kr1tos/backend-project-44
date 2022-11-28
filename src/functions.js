352ff9c29b964f405dde20509e7161d2ac8942ae
Kr1tos <kritos.biz@gmail.com>
2022-11-28 21:41:17 +0300

    Fix

--- src/functions.js
+++ src/functions.js
@@ -1,60 +1,59 @@
-import {car, cdr} from '@hexlet/pairs';
+import { car, cdr } from '@hexlet/pairs';
 
 class func {
+  getRandomNumber(min, max) {
+    const rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+  }
 
-    getRandomNumber (min, max){
-        let rand = this.min - 0.5 + Math.random() * (this.max - this.min + 1);
-        return Math.round(rand);
-    };
+  calculate(operands, operator) {
+    switch (operator) {
+      case '-':
+        return car(operands) - cdr(operands);
+      case '+':
+        return car(operands) + cdr(operands);
+      case '*':
+        return car(operands) * cdr(operands);
+    }
+  }
 
-    calculate(operands, operator){
-        switch (operator) {
-            case '-':
-                return car(operands) - cdr(operands);
-            case '+':
-                return car(operands) + cdr(operands);
-            case '*':
-                return car(operands) * cdr(operands);
-        }
-    };
+  isEven = (number) => number % 2 === 0;
 
-    isEven = (number) => number % 2 === 0;
+  gcd(a, b) {
+    if (b === 0) {
+      return a;
+    }
+    return new func().gcd(b, a % b);
+  }
 
-    gcd(a, b) {
-        if (b === 0) {
-            return a
-        }
-        return new func().gcd(b, a % b)
-    };
+  gemerateProgression(first, step, length) {
+    const nums = [];
+    let count = 0;
+    while (count != length) {
+      const number = first + step * count;
+      nums.push(number);
+      count += 1;
+    }
+    return nums;
+  }
 
-    gemerateProgression(first, step, length){
-        const nums= [];
-        let count = 0;
-        while(count != length) {
-            const number = first + step * count;
-            nums.push(number);
-            count +=1;
-        }
-        return nums
-    };
-    putMissingValue = (missingValue, progression) => {
-        const newProgression = [];
-      
-        for (let index = 0; index < progression.length; index += 1) {
-            if (index === missingValue) {
-                newProgression.push('..');
-            } else {
-                newProgression.push(progression[index]);ы
-            }
-        }
-        return newProgression.join(' ');
+  putMissingValue = (missingValue, progression) => {
+    const newProgression = [];
 
-    };
-    isPrime(num) {
-        for(var i = 2; i < num; i++)
-            if(num % i === 0) return false;
-        return true;
-    };
+    for (let index = 0; index < progression.length; index += 1) {
+      if (index === missingValue) {
+        newProgression.push('..');
+      } else {
+        newProgression.push(progression[index]); 
+      }
+    }
+    return newProgression.join(' ');
+  };
+
+  isPrime(num) {
+    for (let i = 2; i < num; i++) { if (num % i === 0) return false; }
+    return true;
+  }
 }
 
 export default new func();
